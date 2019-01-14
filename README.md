56a8276ab02f11277dac794ff71dfb5ccdc63b10
Jordan Ell <jordan.ell7@gmail.com>
2019-01-14 10:50:25 -0800

    Bumped version to 1.0.0

--- README.md
+++ README.md
@@ -62,12 +62,135 @@ end
 client.list_jobs()
 ```
 
+### Create a Job
+
+- **job** - *hash* - The attributes of the job to create
+
+```ruby
+client.create_job(job: { profile_id: 123 })
+```
+
 ### Get a Job
 
 - **id** - *integer* - The ID of the job
 
 ```ruby
 client.get_job(id: 123)
+```
+
+### Update a Job
+
+- **id** - *integer* - The ID of the job to update
+- **job** - *hash* - The attributes of the hob to update
+
+```ruby
+client.update_job(id: 123, job: { profile_id: 456 })
+```
+
+### Delete a Job
+
+- **id** - *integer* - The ID of the job to delete
+
+```ruby
+client.delete_job(id: 123)
+```
+
+### Process a Job
+
+- **id** - *integer* - The ID of the job to process
+
+```ruby
+client.process_job(id: 123)
+```
+
+### Cancel a Job
+
+- **id** - *integer* - The ID of the job to cancel
+
+```ruby
+client.cancel_job(id: 123)
+```
+
+### List all Profiles
+
+- **page** - *integer* - The page of results to return
+
+```ruby
+client.list_profiles()
+```
+
+### Create a Profile
+
+- **profile** - *hash* - The attributes of the profile to create
+
+```ruby
+client.create_profile(profile: { profile_id: 123 })
+```
+
+### Get a Profile
+
+- **id** - *integer* - The ID of the profile
+
+```ruby
+client.get_profile(id: 123)
+```
+
+### Update a Profile
+
+- **id** - *integer* - The ID of the profile to update
+- **profile** - *hash* - The attributes of the hob to update
+
+```ruby
+client.update_profile(id: 123, profile: { profile_id: 456 })
+```
+
+### Delete a Profile
+
+- **id** - *integer* - The ID of the profile to delete
+
+```ruby
+client.delete_profile(id: 123)
+```
+
+### List all Photos
+
+- **page** - *integer* - The page of results to return
+
+```ruby
+client.list_photos()
+```
+
+### Create a Photo
+
+- **photo** - *hash* - The attributes of the photo to create
+
+```ruby
+client.create_photo(photo: { photo_id: 123 })
+```
+
+### Get a Photo
+
+- **id** - *integer* - The ID of the photo
+
+```ruby
+client.get_photo(id: 123)
+```
+
+### Update a Photo
+
+- **id** - *integer* - The ID of the photo to update
+- **photo** - *hash* - The attributes of the hob to update
+
+```ruby
+client.update_photo(id: 123, photo: { photo_id: 456 })
+```
+
+### Delete a Photo
+
+- **id** - *integer* - The ID of the photo to delete
+
+```ruby
+client.delete_photo(id: 123)
 ```
 
 ## Errors
--- lib/skylab/client.rb
+++ lib/skylab/client.rb
@@ -31,12 +31,131 @@ module Skylab
       Skylab::APIRequest.new(@configuration).get(:jobs, payload)
     end
 
+    def create_job(options = {})
+      raise Skylab::ClientNilArgument, 'job cannot be nil' if options[:job].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).post(:jobs, payload)
+    end
+
     def get_job(options = {})
       raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
 
       payload = options
 
-      Skylab::APIRequest.new(@configuration).get(:jobs, payload)
+      Skylab::APIRequest.new(@configuration).get("jobs/#{options[:id]}", payload)
+    end
+
+    def update_job(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+      raise Skylab::ClientNilArgument, 'job cannot be nil' if options[:job].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).patch("jobs/#{options[:id]}", payload)
+    end
+
+    def delete_job(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).delete("jobs/#{options[:id]}", payload)
+    end
+
+    def process_job(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).post("jobs/#{options[:id]}/process", payload)
+    end
+
+    def cancel_job(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).post("jobs/#{options[:id]}/cancel", payload)
+    end
+
+    def list_profiles(options = {})
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).get(:profiles, payload)
+    end
+
+    def create_profile(options = {})
+      raise Skylab::ClientNilArgument, 'profile cannot be nil' if options[:profile].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).post(:profiles, payload)
+    end
+
+    def get_profile(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).get("profiles/#{options[:id]}", payload)
+    end
+
+    def update_profile(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+      raise Skylab::ClientNilArgument, 'profile cannot be nil' if options[:profile].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).patch("profiles/#{options[:id]}", payload)
+    end
+
+    def delete_profile(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).delete("profiles/#{options[:id]}", payload)
+    end
+
+    def list_photos(options = {})
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).get(:photos, payload)
+    end
+
+    def create_photo(options = {})
+      raise Skylab::ClientNilArgument, 'photo cannot be nil' if options[:photo].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).post(:photos, payload)
+    end
+
+    def get_photo(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).get("photos/#{options[:id]}", payload)
+    end
+
+    def update_photo(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+      raise Skylab::ClientNilArgument, 'photo cannot be nil' if options[:photo].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).patch("photos/#{options[:id]}", payload)
+    end
+
+    def delete_photo(options = {})
+      raise Skylab::ClientNilArgument, 'id cannot be nil' if options[:id].nil?
+
+      payload = options
+
+      Skylab::APIRequest.new(@configuration).delete("photos/#{options[:id]}", payload)
     end
   end
 end
